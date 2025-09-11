b528c6ed6cbb1f4fa08e7add3a2d4775c30a1efd
Luis <luissoca340@gmail.com>
2025-09-10 20:05:11 -0500

    Añadido endpoint create, requirements.txt y README actualizada

--- backend/calendario/urls.py
+++ backend/calendario/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('calendars/', views.calendario, name='calendars'),
     path('calendars/webhooks/calendars/appointments/api/', views.calendario_create_api, name='calendars_create_api'),
     path('calendars/<str:id>/', views.calendario_detail, name='calendars_detail'),
+        path("create/", views.calendario_create_api, name="calendars_create_api"),
 ]
